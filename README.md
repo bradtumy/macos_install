ff88793d2b4cd44796296e8e4293c584987272cf
Brad Tumy <1467134+bradtumy@users.noreply.github.com>
2022-08-05 11:54:38 -0400

    moved instructions over to shell file

--- README.md
+++ README.md
@@ -7,30 +7,4 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## Install packages from homebrew:
-* brew install git
-* brew install maven 
-* brew install ansible
-* brew install kubernetes-cli
-* brew install skaffold
-* brew install kustomize
-* brew install kubectx 
-* brew install gcc
-* brew install minikube
-* brew install kubernetes-cli
-* brew install tmux
 
-* brew cask install virtualbox
-* brew cask install vagrant
-* brew cask install vagrant-manager
-* brew install --cask alfred
-* brew install --cask postman
-* brew cask install docker
-* brew install --cask visual-studio-code
-* brew cask install iterm2
-* brew install --cask apache-directory-studio
-* brew install --cask spectacle
-* brew install --cask hazel
-* brew install --cask telegram
-* brew install --cask slack
-
-* pip3 install openshift 
---
+++ install.sh
@@ -0,0 +1,43 @@
+# Install X Code 
+xcode-select --install
+
+# Install Homebrew:
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Development 
+brew install git
+brew install node
+brew install golang
+brew install — cask docker
+brew install --cask postman
+brew install --cask visual-studio-code
+
+## Communications
+brew install --cask slack
+brew install --cask telegram
+brew install — cask iterm2
+
+## Other
+brew install ansible
+brew install gcc
+# brew install jmeter
+# brew install kubernetes-cli
+# brew install kubectx 
+# brew install kustomize
+# brew install maven
+brew install minikube
+# brew install mitmproxy (optional … only if you need a proxy server)
+# brew install mysql. (First commands:  brew services start mysql … mysql -uroot)
+brew install nginx
+# brew install skaffold
+brew install tmux
+
+## Utility
+brew install --cask alfred
+brew install --cask spectacle
+brew install --cask apache-directory-studio
+brew install --cask atom
+brew install --cask hazel
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install virtualbox
