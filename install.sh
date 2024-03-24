1c2735c5683304fa132e067d7add5be616adb7f7
Brad Tumy <1467134+bradtumy@users.noreply.github.com>
2024-03-24 13:55:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,21 +11,21 @@ brew install git
 brew install node
 brew install gcc
 brew install golang
-brew install kubernetes-cli
-brew install kubectx 
-brew install kustomize
-brew install minikube
-brew install nginx
+#brew install kubernetes-cli
+#brew install kubectx 
+#brew install kustomize
+#brew install minikube
+#brew install nginx
 brew install —-cask docker
 brew install --cask postman
 brew install --cask visual-studio-code
 brew install --cask virtualbox
-brew install --cask neo4j
+#brew install --cask neo4j
 
 ## Automation
-brew install ansible
-brew install --cask vagrant
-brew install --cask vagrant-manager
+#brew install ansible
+#brew install --cask vagrant
+#brew install --cask vagrant-manager
 
 # brew install maven
 # brew install mitmproxy (optional … only if you need a proxy server)
@@ -36,10 +36,10 @@ brew install --cask vagrant-manager
 ## Utilities
 brew install --cask alfred
 brew install --cask spectacle
-brew install --cask apache-directory-studio
-brew install --cask atom
-brew install --cask hazel
+#brew install --cask apache-directory-studio
+#brew install --cask atom
+#brew install --cask hazel
 brew install --cask vlc
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install virtualbox
+#brew cask install vagrant
+#brew cask install vagrant-manager
+#brew cask install virtualbox
