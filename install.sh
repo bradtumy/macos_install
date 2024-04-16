ec76fb0ece9f4c9c48b74c556eee4c53902f7867
Brad Tumy <1467134+bradtumy@users.noreply.github.com>
2024-04-16 03:33:13 -0400

    Update install.sh
    removed unused apps

--- install.sh
+++ install.sh
@@ -11,35 +11,37 @@ brew install git
 brew install node
 brew install gcc
 brew install golang
+brew install —-cask docker
+brew install --cask postman
+brew install --cask visual-studio-code
+brew install --cask virtualbox
+
+## Automation
+
+## Utilities
+brew install --cask alfred
+brew install --cask spectacle
+brew install --cask vlc
+
+
+## Archive
 #brew install kubernetes-cli
 #brew install kubectx 
 #brew install kustomize
 #brew install minikube
 #brew install nginx
-brew install —-cask docker
-brew install --cask postman
-brew install --cask visual-studio-code
-brew install --cask virtualbox
 #brew install --cask neo4j
-
-## Automation
 #brew install ansible
 #brew install --cask vagrant
 #brew install --cask vagrant-manager
-
-# brew install maven
-# brew install mitmproxy (optional … only if you need a proxy server)
-# brew install mysql. (First commands:  brew services start mysql … mysql -uroot)
-# brew install skaffold
-# brew install jmeter
-
-## Utilities
-brew install --cask alfred
-brew install --cask spectacle
-#brew install --cask apache-directory-studio
-#brew install --cask atom
-#brew install --cask hazel
-brew install --cask vlc
+#brew install maven
+#brew install mitmproxy (optional … only if you need a proxy server)
+#brew install mysql. (First commands:  brew services start mysql … mysql -uroot)
+#brew install skaffold
+#brew install jmeter
 #brew cask install vagrant
 #brew cask install vagrant-manager
 #brew cask install virtualbox
+#brew install --cask apache-directory-studio
+#brew install --cask atom
+#brew install --cask hazel
