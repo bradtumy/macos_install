f1f3fdee12a4f4338144eaebb78bc3b6d9821b95
Brad Tumy <brad.tumy@indykite.com>
2025-01-24 13:26:47 -0500

    cleaning up and archiving packages I am no longer using

--- install.sh
+++ install.sh
@@ -8,44 +8,38 @@ xcode-select --install
 
 ## Development 
 brew install git
-brew install node
 brew install gcc
 brew install golang
 brew install —-cask docker
 brew install --cask postman
 brew install --cask visual-studio-code
-brew install --cask virtualbox
-
-## Automation
 
 ## Utilities
 brew install --cask alfred
-brew install --cask notion
-brew install --cask obs
-brew install --cask rectangle
-brew install --cask spectacle
-brew install --cask telegram
-brew install --cask vlc
-
 
 ## Archive
-#brew install kubernetes-cli
-#brew install kubectx 
-#brew install kustomize
-#brew install minikube
-#brew install nginx
-#brew install --cask neo4j
 #brew install ansible
-#brew install --cask vagrant
-#brew install --cask vagrant-manager
-#brew install maven
-#brew install mitmproxy (optional … only if you need a proxy server)
-#brew install mysql. (First commands:  brew services start mysql … mysql -uroot)
-#brew install skaffold
 #brew install jmeter
-#brew cask install vagrant
-#brew cask install vagrant-manager
-#brew cask install virtualbox
+#brew install kubectx 
+#brew install kubernetes-cli
+#brew install kustomize
+#brew install maven
+#brew install minikube
+#brew install mitmproxy #(optional … only if you need a proxy server)
+#brew install mysql. #(First commands:  brew services start mysql … mysql -uroot)
+#brew install nginx
+#brew install node
+#brew install skaffold
+
 #brew install --cask apache-directory-studio
 #brew install --cask atom
 #brew install --cask hazel
+#brew install --cask neo4j
+#brew install --cask notion
+#brew install --cask obs
+#brew install --cask rectangle
+#brew install --cask spectacle
+#brew install --cask vagrant
+#brew install --cask vagrant-manager
+#brew install --cask virtualbox
+#brew install --cask vlc
